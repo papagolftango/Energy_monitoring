1d95941bc4cc379e238d74836d5ca234f8d5d646
paul@thetindalls.co.uk <TindallP@SEPURA.co.uk>
2022-09-28 10:46:59 +0100

    added BDD Tests

---
+++ features/connection.feature
@@ -0,0 +1,6 @@
+Feature: Startup
+
+Scenario: Startup
+        Given the device is operation
+        When its switched operation
+        Then the LED lights
---
+++ features/steps/connsteps.py
@@ -0,0 +1,16 @@
+from behave import *
+
+
+@given(u'the device is operation')
+def step_impl(context):
+    return(True)
+
+
+@when(u'its switched operation')
+def step_impl(context):
+    return(True)
+
+
+@then(u'the LED lights')
+def step_impl(context):
+    return(True)
