16c6adaf88f281ca523ab157d335aacf91c4d547
papagolftango <paul@thetindalls.co.uk>
2024-06-29 12:37:07 +0100

    stuff

--- gauge.py
+++ gauge.py
@@ -1,48 +1,42 @@
 import time
-from motor import *
-# import bme280
+from motor import Motor
 
 class GaugeStepper:
-     __current_val = 0
-     __current_steps = 0
-     __calibrated = False
-     __m = 0
-     __scale = 1.0
-     __maxStep = 6000
-        
-     def __init__(self, name, min_val, max_val, id, m):
-        self.name = name
-        self.max_val = max_val
-        self.min_val = min_val
-        self.id = m
-        self.__m = m
-        __current_val = min_val
-        __step = 0
-        __calibrated = False
-       # self.__m.Reset()
-     
-      
-     # using the gauge's scaled units, work out where to move in steps and move 
-     def MoveTo(self, position):
-        if (position > self.max_val) :
-           position = self.max_val
-        elif (position < self.min_val) :
-           position = self.min_val  
-        delta = position - self.__current_val
-        self.__m.move(self.id,delta*self.__scale)   
-        self.__current_val = position
+   def __init__(self, name, min_val, max_val, id, motor):
+      self.name = name
+      self.max_val = max_val
+      self.min_val = min_val
+      self.id = id
+      self.__motor = motor
+      self.__current_val = min_val  # Initialize current value to min_val
+      self.__current_steps = 0  # Initialize current steps to 0
+      self.__calibrated = False  # Initialize calibration status to False
+      self.__scale = 1.0  # Initialize scale to 1.0
+      self.__max_step = 6000  # Initialize max steps to 6000
 
-     def GetPos(self):
-       return(self.__current_val)
-   
+   def move_to(self, position):
+      """Move the gauge to a specified position within its range."""
+      if position > self.max_val:
+         position = self.max_val
+      elif position < self.min_val:
+         position = self.min_val
+      delta = position - self.__current_val
+      self.__motor.move(self.id, delta * self.__scale)
+      self.__current_val = position
 
-     def Calibrate(self):
-        self.__m.move(self.id, self.__maxStep)
-        self.__m.move(self.id, 0)
-        self.__current_val = self.min_val
-        self.__scale = (self.__maxStep - 0)/(self.max_val - self.min_val)
-        self.__calibrated = True
-        print(self.__scale)
-        
-     def GetStatus(self):
-        return(self.__current_val, self.__calibrated)
+   def get_pos(self):
+      """Return the current position of the gauge."""
+      return self.__current_val
+
+   def calibrate(self):
+      """Calibrate the gauge, setting its scale based on its range and max steps."""
+      self.__motor.move(self.id, self.__max_step)
+      self.__motor.move(self.id, 0)
+      self.__current_val = self.min_val
+      self.__scale = self.__max_step / (self.max_val - self.min_val)
+      self.__calibrated = True
+      print(f"Calibration scale set to: {self.__scale}")
+
+   def get_status(self):
+      """Return the current position and calibration status of the gauge."""
+      return self.__current_val, self.__calibrated
--- mqttgauge.py
+++ mqttgauge.py
@@ -4,89 +4,61 @@ import argparse
 from gauge import *
 
 # Parse command-line arguments for GPIO pins and motor ID
-
 parser = argparse.ArgumentParser()
-parser.add_argument('motorID', type=str,  help='Unique motor identifier')
-parser.add_argument('minGauge', type=int,  help='min value for gauge position')
-parser.add_argument('maxGauge', type=int,  help='max value for gauge position')
+parser.add_argument('motorID', type=str, help='Unique motor identifier')
+parser.add_argument('minGauge', type=int, help='min value for gauge position')
+parser.add_argument('maxGauge', type=int, help='max value for gauge position')
 args = parser.parse_args()
 
-
-# create a gauge instance
+# Create a gauge instance
 g = GaugeStepper(args.motorID, args.minGauge, args.maxGauge)
 
 # MQTT settings
 MQTT_BROKER = '192.168.68.96'
 MQTT_PORT = 1883
-MQTT_TOPIC_TEMPLATE = 'PowerGauge/{}  '  # Topic template with motor ID placeholder
+MQTT_BASE_TOPIC = 'PowerGauge/Motors/{}'.format(args.motorID)  # Base topic with motor ID
 
-topic = MQTT_TOPIC_TEMPLATE.format(args.motorID)
-print(topic)
-
-def status(param):
-  print( "Status", param) 
-  g.GetStatus()
+def status(param, motor_instance):
+    print("Status", param)
+    motor_instance.GetStatus()
   
-def setup(param):
-  print ( "Setup", param)
-  g.Calibrate()
+def setup(param, motor_instance):
+    print("Setup", param)
+    motor_instance.Calibrate()
 
-def getpos(param):
-   print ("GetPos", param)
-   print(g.getpos())
+def getpos(param, motor_instance):
+    print("GetPos", param)
+    print(motor_instance.getpos())
 
-def move(param):
-  print ("Move", param)
-  g.MoveTo(param)
+def move(param, motor_instance):
+    print("Move", param)
+    motor_instance.MoveTo(param)
 
 Cmds = {
-        "status" : status,
-        "setup" : setup,
-        "version": getpos,
-        "move" :  move,
-      }
+    "status": status,
+    "setup": setup,
+    "getPosition": getpos,
+    "moveTo": move,
+}
 
+
+#
 def on_message(client, userdata, msg):
-    print(msg)
-    topic_parts = msg['topic'].split('/')
-    if len(topic_parts) == 2 and topic_parts[0] == MQTT_TOPIC_TEMPLATE:
-       motor_name = topic_parts[1]
-       # Perform the reverse lookup to get the motor instance
-       motor_instance = find_motor_by_name(motor_name, gauges)
-       if motor_instance is not None:
-         parts = msg["payload"].split(" ")
-         if len(parts) >= 2:
-            Cmds[parts[0]](parts[1], motor_instance)
-         elif len(parts) == 1:
-        # Handle the case where there's only one item
-            Cmds[parts[0]](motor_instance)
-         else:
-           pass
-       else:
-         print(f"No motor instance found for '{motor_name}'")
-    else:
-        print("Invalid MQTT topic format")
-        
+    print(msg.topic + " " + str(msg.payload))
+    topic_parts = msg.topic.split('/')
+    if len(topic_parts) == 4 and topic_parts[2] == args.motorID:
+        command = topic_parts[3]
+        payload = msg.payload.decode('utf-8')
+        if command in Cmds:
+            Cmds[command](payload, g)
+        else:
+            print("Unknown command")
 
-def on_connect(client, userdata, flags, rc):
-  print("Connected with result code "+str(rc))
-  client.subscribe(topic)
-
-
-# Connect to MQTT broker and subscribe to topic
+# MQTT Client Setup
 client = mqtt.Client()
-client.username_pw_set("pgt", "2906")
 client.on_message = on_message
-client.connect(MQTT_BROKER, MQTT_PORT)
+client.connect(MQTT_BROKER, MQTT_PORT, 60)
 
-client.loop_start()
-# Main loop (can be replaced with event-driven mechanisms)
-try:
-    while True:
-        pass  # Keep the program running
-except KeyboardInterrupt:
-    # Clean up
-    client.disconnect()
-    g.Finish()
-
-
+# Subscribe to all commands for this motor
+client.subscribe(MQTT_BASE_TOPIC + '/#')
+client.loop_forever()
